50811919adebc234eecb48e6c84aecc715ead443
pphila <efl2ice89@gmail.com>
2022-04-11 20:44:42 -0700

    add second analyzer function and add more test conditions
    Co-authored-by: Co-authored-by: Megan McKissack <meganmckissack@gmail.com>

--- js/scritps.js
+++ js/scritps.js
@@ -13,3 +13,16 @@ function wordCounter(text) {
   return wordCount;
 }
 
+function numberOfOccurrencesInText(word, text) {
+  if (text.trim().length === 0) {
+    return 0;
+  }
+  const wordArray = text.split(" ");
+  let wordCount = 0;
+  wordArray.forEach(function(element){
+    if (word.toLowerCase() === element.toLowerCase()) {
+      wordCount++
+    }
+  });
+  return wordCount;
+}
